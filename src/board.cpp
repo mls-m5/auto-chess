fb85277f41ea38a8b251eb92f0d04d63f53091e4
Mattias Lasersköld <mattias.l.sk@gmail.com>
2019-11-20 22:07:51 +0100

    Added validity checks for the last pieces

--- src/board.cpp
+++ src/board.cpp
@@ -55,6 +55,56 @@ public:
 		return forwardDirection(_currentPlayer);
 	}
 
+	//! Checksi lines and diagonals if it is okay to move
+	bool isPathClear(int fromX, int fromY, int toX, int toY) {
+		auto dx = fromX - toX;
+		auto dy = fromY - toY;
+		if (dx == 0) {
+			// Vertical movement
+			auto length = abs(dy);
+			auto direction = dy / length;
+			for (int i = 0; i < length; ++i) {
+				auto ty = fromY + i * direction;
+				if (get(fromX, ty).type != 0) {
+					return false;
+				}
+			}
+		}
+		else if (dy == 0) {
+			// Horizontal movement
+			auto length = abs(dx);
+			auto direction = dx / length;
+			for (int i = 0; i < length; ++i) {
+				auto tx = fromX + i * direction;
+				if (get(tx, fromY).type != 0) {
+					return false;
+				}
+			}
+		}
+		else if (abs(dx) == abs(dy)) {
+			auto length = abs(dx); // Note that |dx| = |dy|
+			auto dirx = dx / length;
+			auto diry = dy / length;
+
+			for (int i = 0; i < length; ++i) {
+				auto tx = fromX + i * dirx;
+				auto ty = fromY + i * diry;
+				if (get(tx, ty).type != 0) {
+					return false;
+				}
+			}
+		}
+		else {
+			return false; // The path is not straight
+		}
+		auto targetCell = get(toX, toY);
+
+		if (targetCell.type != 0 && targetCell.player == _currentPlayer) {
+			return false;
+		}
+		return true;
+	}
+
 	bool isMoveValid(int fromX, int fromY, int toX, int toY) {
 		if (!isInside(fromX, fromY)) {
 			return false;
@@ -107,11 +157,29 @@ public:
 				return true;
 			}
 		}
+		else if (fromCell.type == 'k') {
+			if (abs(toX - fromX) <= 1 && abs(toY - toY) <= 1) {
+				return true;
+			}
+		}
+		else if (fromCell.type == 'r') {
+			if (toX == fromX || toY == fromY) {
+				return isPathClear(fromX, fromY, toX, toY);
+			}
+		}
+		else if (fromCell.type == 'b') {
+			if (abs(toX - fromX) == abs(toY - fromY)) {
+				return isPathClear(fromX, fromY, toX, toY);
+			}
+		}
+		else if (fromCell.type == 'q') {
+			return isPathClear(fromX, fromY, toX, toY);
+		}
 
 		return false;
 	}
 
-	//! Move a piece and return true if successfull
+	//! Move a piece and return true if successful
 	bool move(int fromX, int fromY, int toX, int toY) override {
 		if (isMoveValid(fromX, fromY, toX, toY)) {
 			(*this)(toX, toY) = {};
